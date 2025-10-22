1de782a35f66f456575e59a8185a3253a7cd2bf5
Michele Invernizzi <michele@peptone.io>
2025-10-22 11:04:54 +0000

    change default usage to make it easier for new models

--- forward-models/run_forward_model.py
+++ forward-models/run_forward_model.py
@@ -17,8 +17,8 @@ import logging
 import os
 import shutil
 from glob import glob
-from multiprocessing import Pool
 
+import joblib
 import mdtraj as md
 import pandas as pd
 
@@ -28,15 +28,17 @@ logger = logging.getLogger(__name__)
 from cspred_wrapper import run_cspred
 from pepsi_wrapper import run_pepsi
 
+N_JOBS = min(joblib.cpu_count(), int(os.getenv("N_JOBS", "64")))
+
 
 def get_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
-        description="Run forward model calculations on ensembles generated with different generators."
-        " If ensembles are already in XTC+PDB format, use --no-prepare-ensembles to skip conversion step, "
-        "and set --output-dir to the same directory containing the ensembles (--generators-dir).",
+        description="Run forward model calculations on protein ensembles."
+        "Set number of processes with N_JOBS env variable.",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument(
+        "-p",
         "--predictor",
         type=str,
         choices=["Pepsi", "UCBshift", "Sparta+", "UCBshift_Sparta+"],
@@ -44,31 +46,17 @@ def get_args() -> argparse.Namespace:
         help="Forward-model predictor to use",
     )
     parser.add_argument(
-        "--generators-dir",
-        help="directory containing one folder for each generator",
-        default="./generators",
-    )
-    parser.add_argument(
-        "--generators",
-        help="generators to post-process, comma separated",
-        default="",
-    )
-    parser.add_argument(
-        "--dataset",
-        help="path to PeptoneDB csv, leave empty to use default CS and SAXS locations",
+        "-f",
+        "--generator-dir",
         type=str,
-        default=None,
+        nargs="+",
+        default=["."],
+        help="folder(s) with generated protein ensembles to be analyzed and reweighted",
     )
     parser.add_argument(
         "--output-dir",
-        help="directory where to store results",
-        default="./processed",
-    )
-    parser.add_argument(
-        "--nprocs",
-        help="number of processors to use",
-        type=int,
-        default=8,
+        help="directory where to store results. Default is to use the same directory as the generator",
+        default=None,
     )
     parser.add_argument(
         "--prepare-ensembles",
@@ -79,20 +67,34 @@ def get_args() -> argparse.Namespace:
         "--no-prepare-ensembles",
         action="store_false",
         dest="prepare_ensembles",
-        help="Do not prepare ensembles",
+        help="Do not prepare ensembles, they are already in XTC+PDB format",
     )
-    parser.set_defaults(prepare_ensembles=True)
+    parser.set_defaults(prepare_ensembles=False)
+    parser.add_argument(
+        "--dataset",
+        help="path to PeptoneDB csv, leave empty to use default CS and SAXS locations",
+        type=str,
+        default=None,
+    )
+    parser.add_argument("--overwrite", action="store_true", help="Overwrite existing forward model prediction files")
 
     # SAXS-related arguments
     parser.add_argument(
         "--saxs-data",
-        help="path to SAXS experimental data",
+        help="path to SAXS experimental data, needed to run Pepsi",
         type=str,
         default="/app/peptonebench/datasets/PeptoneDB-SAXS/sasbdb-clean_data",
     )
     parser.add_argument(
+        "--saxs-filename",
+        help="filename inside saxs_data folder. "
+        "PeptoneBD-CS uses 'LABEL-bift.dat', while PeptoneDB-Integrative uses 'LABEL/SAXS_bift.dat'",
+        type=str,
+        default="LABEL-bift.dat",
+    )
+    parser.add_argument(
         "--pepsi-path",
-        help="path to pepsi executable",
+        help="path to Pepsi-SAXS executable",
         type=str,
         default="/home/mambauser/Pepsi-SAXS",
     )
@@ -110,11 +112,12 @@ def save_as_xtc_and_pdb(filename: str, generator_dir: str, output_dir: str, labe
     trj.save(os.path.join(output_dir, label + ".xtc"))
 
 
-def prepare_ensembles_custom(generator_name: str, generator_dir: str, output_dir: str) -> None:
+def prepare_ensembles_custom(generator_dir: str, output_dir: str, generator_name: str = None) -> None:
     """Prepare ensemble files from different generators saving them into XTC + PDB format.
     Supported generators: 
-    - 'alphafold': We only evaluate the 1st ranked alphafold prediction. This code assumes alphafold2 was ran using the public colabfold available at
-    https://colab.research.google.com/github/sokrypton/ColabFold/blob/main/batch/AlphaFold2_batch.ipynb
+    - 'alphafold': We only evaluate the 1st ranked alphafold prediction. 
+        This code assumes alphafold2 was ran using the public colabfold available at
+        https://colab.research.google.com/github/sokrypton/ColabFold/blob/main/batch/AlphaFold2_batch.ipynb
     - 'bioemu': This code assumes bioemu was run as follows:
         while  IFS=, read -r label sequence ph gscores ; do
             python -m bioemu.sample --sequence "$sequence" --num_samples $num_samples --filter_samples=False \
@@ -144,6 +147,8 @@ def prepare_ensembles_custom(generator_name: str, generator_dir: str, output_dir
         done < PeptoneDB.csv
     - 'peptron': trajectories in LABEL.pdb format
     """
+    if generator_name is None:
+        generator_name = os.path.basename(generator_dir)
     if generator_name == "alphafold":
         for pdb_file in sorted(
             glob(f"{generator_dir}/*_unrelaxed_rank_001_alphafold2_ptm_model_*.pdb"),
@@ -192,31 +197,40 @@ def prepare_ensembles_custom(generator_name: str, generator_dir: str, output_dir
 
 def process_generator(
     predictor: str,
-    generators_dir: str,
-    generator_name: str,
+    generator_dir: str,
     output_dir: str,
     db: pd.DataFrame,
     prepare_ensembles: bool = True,
-    nproc: int = 8,
+    overwrite: bool = False,
     saxs_data: str = "",
+    saxs_filename: str = "LABEL-bift.dat",
     pepsi_path: str = "",
 ) -> None:
     """
-    Parallely (nproc processes) runs the predictor forward models on ensembles from a single
-    protein generator
+    Runs the selected predictor forward model on ensembles from a single protein generator directory.
 
-    :param predictor: which forward-model predictor to use
-    :param generators_dir: directory containing one folder for each generator
-    :param generator_name: name of the generator to be processed
-    :param output_dir: results are written to output_dir/generator_name
-    :param db: dictionary containing sequences and pH levels for each label
-    :param nproc: how many processes to use
+    Ensembles are optionally prepared (converted to XTC+PDB format) and processed in parallel using N_JOBS.
+    Results are written to the specified output directory.
+
+    :param predictor: Forward-model predictor to use ("Pepsi", "UCBshift", "Sparta+", "UCBshift_Sparta+")
+    :param generator_dir: Directory containing generated protein ensembles
+    :param output_dir: Directory to store results
+    :param db: Pandas DataFrame containing sequences and pH levels for each label
+    :param prepare_ensembles: Whether to prepare ensembles into XTC+PDB format
+    :param overwrite: Overwrite existing prediction files
+    :param saxs_data: Path to SAXS experimental data (for Pepsi)
+    :param saxs_filename: Filename pattern for SAXS data (for Pepsi)
+    :param pepsi_path: Path to Pepsi-SAXS executable
     """
-    logger.info(f"processing {generator_name} with {predictor}")
-    generator_dir = os.path.join(generators_dir, generator_name)
+    logger.info(f"--- processing '{generator_dir}' with '{predictor}' ---")
     os.makedirs(output_dir, exist_ok=True)
     if prepare_ensembles:
-        prepare_ensembles_custom(generator_name, generator_dir, output_dir)
+        logger.info(
+            f"prepare_ensembles=True: copying ensembles from '{generator_dir}' to '{output_dir}' in XTC+PDB format",
+        )
+        prepare_ensembles_custom(generator_dir, output_dir)
+    else:
+        logger.info(f"prepare_ensembles=False: expecting XTC+PDB ensembles in '{generator_dir}'")
 
     if predictor == "Pepsi":
 
@@ -224,12 +238,13 @@ def process_generator(
             return run_pepsi(
                 trajectory=os.path.join(generator_dir, f"{label}.xtc"),
                 topology=os.path.join(generator_dir, f"{label}.pdb"),
-                saxs=saxs_data,
+                saxs=os.path.join(saxs_data, saxs_filename.replace("LABEL", label)),
                 output=output_dir,
                 pepsi=pepsi_path,
                 pH=db.loc[label, "pH"],
                 sequence=db.loc[label, "sequence"],
-                angular_units=1,
+                angular_units=1,  # correct units for bift processed data in PeptoneDB-SAXS and PeptoneDB-Integrative
+                overwrite=overwrite,
             )
     elif predictor in ("UCBshift", "Sparta+", "UCBshift_Sparta+"):
 
@@ -241,25 +256,22 @@ def process_generator(
                 predictor=predictor.split("_"),
                 pH=db.loc[label, "pH"],
                 sequence=db.loc[label, "sequence"],
+                overwrite=overwrite,
             )
     else:
         raise ValueError(f"predictor {predictor} not supported")
 
-    pool = Pool(nproc)
-    pool.map(process_ensemble, sorted(os.listdir(generator_dir)))
+    labels = sorted([os.path.basename(file).replace(".xtc", "") for file in glob(os.path.join(generator_dir, "*.xtc"))])
+    logger.info(f"found {len(labels):_} ensembles to process")
+    if set(labels) - set(db.index):
+        raise ValueError(
+            f"some labels in '{generator_dir}' are not in the provided dataset: {set(labels) - set(db.index)}",
+        )
+    joblib.Parallel(n_jobs=min(N_JOBS, len(labels)))(joblib.delayed(process_ensemble)(label) for label in labels)
 
 
 def main() -> None:
-    try:
-        args = get_args()
-    except SystemExit:
-        raise ValueError("could not parse command-line arguments")
-
-    if (not args.prepare_ensembles) and args.generators_dir != args.output_dir:
-        logger.warning(
-            "--no-prepare-ensembles: set --output-dir to the same directory containing the ensembles (--generators-dir)"
-            " unless you already moved the prepared ensembles to --output-dir",
-        )
+    args = get_args()
     if args.dataset is None:
         if args.predictor == "Pepsi":
             args.dataset = "/app/peptonebench/datasets/PeptoneDB-SAXS/PeptoneDB-SAXS.csv"
@@ -268,25 +280,31 @@ def main() -> None:
         else:
             raise ValueError(f"predictor {args.predictor} not supported")
     db = pd.read_csv(args.dataset, index_col="label")
-    if len(args.generators) > 0:
-        generators = args.generators.split(",")
+    logger.info(f"loaded dataset from {args.dataset} with {len(db)} entries")
+    logger.info(f"processing directories: {args.generator_dir}")
+    if args.overwrite:
+        logger.info("overwrite=True: existing forward model prediction files will be recomputed")
     else:
-        generators = sorted(
-            [d for d in os.listdir(args.generators_dir) if os.path.isdir(os.path.join(args.generators_dir, d))],
-        )
-    logger.info(f"found generators: {generators}")
-    for g in generators:
-        processed_dir = str(os.path.join(args.output_dir, g))
+        logger.info("overwrite=False: existing forward model prediction files will be skipped")
+    if args.prepare_ensembles and args.output_dir is None:
+        default_output_dir = "processed_ensembles"
+        logger.info(f"'prepare_ensembles=True': setting --output-dir to '{default_output_dir}' to avoid overwriting")
+        args.output_dir = default_output_dir
+    for generator_dir in args.generator_dir:
+        if args.output_dir is None:
+            processed_dir = generator_dir
+        else:
+            processed_dir = os.path.join(args.output_dir, os.path.basename(generator_dir))
         os.makedirs(processed_dir, exist_ok=True)
         process_generator(
             predictor=args.predictor,
-            generators_dir=args.generators_dir,
-            generator_name=g,
+            generator_dir=generator_dir,
             output_dir=processed_dir,
             db=db,
-            nproc=args.nprocs,
             prepare_ensembles=args.prepare_ensembles,
+            overwrite=args.overwrite,
             saxs_data=args.saxs_data,
+            saxs_filename=args.saxs_filename,
             pepsi_path=args.pepsi_path,
         )
 
